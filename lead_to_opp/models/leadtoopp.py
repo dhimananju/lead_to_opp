af1ae493582f41db719549ec2f266723dd47d406
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-24 16:57:47 +0530

    Update leadtoopp.py

--- lead_to_opp/models/leadtoopp.py
+++ lead_to_opp/models/leadtoopp.py
@@ -2,7 +2,7 @@ from odoo import models, api
 import logging
 _logger = logging.getLogger(__name__)
 
-class CrmLead(models.TransientModel):
+class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     def action_apply(self):
