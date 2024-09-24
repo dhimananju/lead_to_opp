cddf03a3d1b22d251e2c7417b4b3247f8071c3ce
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-24 16:39:06 +0530

    Update leadtoopp.py

--- lead_to_opp/models/leadtoopp.py
+++ lead_to_opp/models/leadtoopp.py
@@ -13,17 +13,18 @@ class CrmLead(models.TransientModel):
              if(self.action=="create"):
                 _logger.info('Anju second step')
                 _logger.info(self)
-                new_contact = self.env['res.partner'].create({
-                     #'x_studio_email_opt_out': self.x_studio_email_opt_out,
-                     'x_studio_facebook': self.x_studio_facebook,
-                     'x_studio_first_name': self.x_studio_first_name,
-                     'x_studio_last_name': self.x_studio_last_name,
-                     'industry_id': self.x_studio_industry_id,
-                     'x_studio_linkedin_url': self.x_studio_linkedin_url,
-                     'x_studio_type_of_lead': self.x_studio_type_of_lead,
-                     'x_studio_source_id': self.x_studio_source_id | "source",
-                     'x_studio_secondary_email': self.x_studio_secondary_email,
-                })
+                for record in self:
+                    new_contact = self.env['res.partner'].create({
+                         'x_studio_email_opt_out': record.x_studio_email_opt_out,
+                         'x_studio_facebook': record.x_studio_facebook,
+                         'x_studio_first_name': record.x_studio_first_name,
+                         'x_studio_last_name': record.x_studio_last_name,
+                         'industry_id': record.x_studio_industry_id,
+                         'x_studio_linkedin_url': record.x_studio_linkedin_url,
+                         'x_studio_type_of_lead': record.x_studio_type_of_lead,
+                         'x_studio_source_id': record.x_studio_source_id | "source",
+                         'x_studio_secondary_email': record.x_studio_secondary_email,
+                    })
                 # Log the creation for debugging
                 _logger.info('New contact created with ID: %s', new_contact.id)
 
