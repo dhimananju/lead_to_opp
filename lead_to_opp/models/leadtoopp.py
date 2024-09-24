7e3d34d1433695739114a2480e2dcbb5647eb53b
anju-targetintegration <anju.dhiman@targetintegration.com>
2024-09-24 17:44:09 +0530

    Update leadtoopp.py

--- lead_to_opp/models/leadtoopp.py
+++ lead_to_opp/models/leadtoopp.py
@@ -20,29 +20,29 @@ class CrmLead(models.TransientModel):
 
                 # Access the leads selected in the wizard
                 leads = self.env['crm.lead'].browse(self._context.get('active_ids', []))
-                    # Iterate over the leads and access the custom field
-                    for record in leads:
-                        existing_contact = self.env['res.partner'].search([('email', '=', record.email)], limit=1)
-                        if existing_contact:
-                            # If a contact with the same email exists, log and return the existing contact
-                            _logger.info('Contact with email %s already exists. Contact ID: %s', email, existing_contact.id)
-                            return existing_contact
-                        else:
-                            new_contact = self.env['res.partner'].create({
-                                 'name': record.x_studio_first_name + " "+ record.x_studio_last_name,
-                                 'x_studio_email_opt_out': record.x_studio_email_opt_out,
-                                 'x_studio_facebook': record.x_studio_facebook,
-                                 'x_studio_first_name': record.x_studio_first_name,
-                                 'x_studio_last_name': record.x_studio_last_name,
-                                 'industry_id': record.x_studio_industry,
-                                 'x_studio_linkedin_url': record.x_studio_linkedin_url,
-                                 'x_studio_type_of_lead': record.x_studio_type_of_lead,
-                                 'x_studio_source': record.x_studio_source,
-                                 'x_studio_secondary_email': record.x_studio_secondary_email,
-                            })
-                        
-                            # Log the creation for debugging
-                            _logger.info('New contact created with ID: %s', new_contact.id)
+                # Iterate over the leads and access the custom field
+                for record in leads:
+                    existing_contact = self.env['res.partner'].search([('email', '=', record.email)], limit=1)
+                    if existing_contact:
+                        # If a contact with the same email exists, log and return the existing contact
+                        _logger.info('Contact with email %s already exists. Contact ID: %s', email, existing_contact.id)
+                        return existing_contact
+                    else:
+                        new_contact = self.env['res.partner'].create({
+                             'name': record.x_studio_first_name + " "+ record.x_studio_last_name,
+                             'x_studio_email_opt_out': record.x_studio_email_opt_out,
+                             'x_studio_facebook': record.x_studio_facebook,
+                             'x_studio_first_name': record.x_studio_first_name,
+                             'x_studio_last_name': record.x_studio_last_name,
+                             'industry_id': record.x_studio_industry,
+                             'x_studio_linkedin_url': record.x_studio_linkedin_url,
+                             'x_studio_type_of_lead': record.x_studio_type_of_lead,
+                             'x_studio_source': record.x_studio_source,
+                             'x_studio_secondary_email': record.x_studio_secondary_email,
+                        })
+                    
+                        # Log the creation for debugging
+                        _logger.info('New contact created with ID: %s', new_contact.id)
 
 
                  
